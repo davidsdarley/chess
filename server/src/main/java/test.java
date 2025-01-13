22c3cfa11d2e79e0285b2ed89d04088e738a53dd
David <davidsdarley@gmail.com>
2025-01-13 14:07:03 -0700

    java practice

--- server/src/main/java/test.java
+++ server/src/main/java/test.java
@@ -1,5 +1,24 @@
 public class test {
     public static void main(String[] args) {
-        System.out.println("Hello, IntelliJ v3!");
+        Piece pawn1 = new Piece("Knight", 'b');
+        pawn1.move();
+    }
+}
+class Piece{
+    String type;
+    char color;
+
+    public Piece(String type, char color){
+        this.type = type;
+        this.color = color;
+    }
+
+    public void move(){
+        if (this.type == "Pawn"){
+            System.out.println("Move forward one space");
+        } else if (this.type == "Knight") {
+            System.out.println("Move two spaces one direction, one space a different direction");
+        }
+
     }
 }
