19db7a0d16ae21ef9c205a99475a50cd36a3f1b1
David <davidsdarley@gmail.com>
2025-01-16 16:50:54 -0700

    Progress: ChessBoard basics created, ChesPiece stores most info, ChessPosition now keeps track of what piece is inside it

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -1,5 +1,5 @@
 package chess;
-
+import chess.*;
 /**
  * A chessboard that can hold and rearrange chess pieces.
  * <p>
@@ -7,9 +7,16 @@ package chess;
  * signature of the existing methods.
  */
 public class ChessBoard {
+    ChessPosition[][] board = new ChessPosition[8][8];
 
     public ChessBoard() {
-        
+        //assign a ChessPosition to each of the available spaces in the board
+        for(int row = 0; row < 8; row+=1){
+            for(int column = 0; column <8; column +=1){
+                ChessPosition pos =new ChessPosition(row, column);
+                board[row][column] = pos;}
+            }
+        resetBoard();
     }
 
     /**
@@ -19,6 +26,8 @@ public class ChessBoard {
      * @param piece    the piece to add
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
+        position.piece = piece;
+
         throw new RuntimeException("Not implemented");
     }
 
@@ -38,6 +47,59 @@ public class ChessBoard {
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
-        throw new RuntimeException("Not implemented");
+        //set up black in rows 0 and 1
+        //pawns first
+        for (int counter = 0; counter < 8; counter += 1) {
+            ChessPiece pawn = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.PAWN);
+            board[1][counter].piece = pawn;
+        }
+
+        // all the others, in order KING, QUEEN, BISHOPs, KNIGHTs, ROOKs
+        board[0][4].piece = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KING);
+        board[0][3].piece = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.QUEEN);
+        board[0][2].piece = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.BISHOP);
+        board[0][5].piece = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.BISHOP);
+        board[0][1].piece = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KNIGHT);
+        board[0][6].piece = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KNIGHT);
+        board[0][0].piece = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.ROOK);
+        board[0][7].piece = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.ROOK);
+
+        //set up white in rows 6 and 7
+        //pawns first
+        for (int counter = 0; counter < 8; counter += 1) {
+            ChessPiece pawn = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+            board[6][counter].piece = pawn;
+        }
+
+        // all the others, in order KING, QUEEN, BISHOPs, KNIGHTs, ROOKs
+        board[7][4].piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KING);
+        board[7][3].piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.QUEEN);
+        board[7][2].piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.BISHOP);
+        board[7][5].piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.BISHOP);
+        board[7][1].piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KNIGHT);
+        board[7][6].piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KNIGHT);
+        board[7][0].piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.ROOK);
+        board[7][7].piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.ROOK);
     }
+
+    @Override
+    public String toString(){
+        String str = "";
+        for(ChessPosition[] row:this.board){
+            for(ChessPosition place: row){
+                str += place;
+            }
+            str += "\n";
+        }
+        return str;
+    }
+
+        //DEBUG main method for testing purposes
+    public static void main(String[] args){
+
+        ChessBoard tester = new ChessBoard();
+        System.out.println(tester);
+
+    }
+
 }
--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -1,4 +1,7 @@
 package chess;
+import chess.*;
+
+import static chess.ChessPiece.PieceType.*;
 
 /**
  * Represents a single square position on a chess board
@@ -7,8 +10,14 @@ package chess;
  * signature of the existing methods.
  */
 public class ChessPosition {
+    int row;
+    int col;
+    ChessPiece piece;
+
 
     public ChessPosition(int row, int col) {
+        this.row = row;
+        this.col = col;
     }
 
     /**
@@ -16,14 +25,38 @@ public class ChessPosition {
      * 1 codes for the bottom row
      */
     public int getRow() {
-        throw new RuntimeException("Not implemented");
+        return this.row;
     }
-
     /**
      * @return which column this position is in
      * 1 codes for the left row
      */
     public int getColumn() {
-        throw new RuntimeException("Not implemented");
+        return this.col;
     }
+
+    @Override
+    public String toString(){
+        if(this.piece == null){
+            return "| |";
+        }
+        else if(piece.type == KING){
+            return String.format("|K|");
+        }
+        else if(piece.type == QUEEN) {
+            return String.format("|Q|");
+        }
+        else if(piece.type == BISHOP) {
+            return String.format("|B|");
+        }
+        else if(piece.type == KNIGHT){
+                return String.format("|k|");}
+        else if(piece.type == ROOK){
+            return String.format("|R|");}
+        else{
+            return String.format("|P|");
+        }
+        //return String.format("Square (%d, %d)",this.row, this.col);
+    }
+
 }
